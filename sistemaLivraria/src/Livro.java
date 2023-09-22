16826470984a97f155a0cf25051d28dfe0bf7843
Santzu-27 <gabi260402@gmail.com>
2023-09-22 20:38:30 -0300

    comited

---
+++ sistemaLivraria/src/Livro.java
@@ -0,0 +1,16 @@
+public class Livro {
+        // ● Titulo
+    // ● Código
+    // ● Editora
+    // ● Área
+    // ● Ano
+    // ● Valor
+    // ● Quantidade em Estoque
+
+    String titulo, codigo, editora, area;
+    int ano, estoque;
+    double valor;
+
+    
+
+}

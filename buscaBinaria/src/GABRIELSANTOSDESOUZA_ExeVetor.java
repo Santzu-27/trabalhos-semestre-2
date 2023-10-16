34bacb2d55cef957b32a6c2dba700d8609c06326
Santzu-27 <gabi260402@gmail.com>
2023-10-16 16:09:37 -0300

    file update

--- buscaBinaria/src/GABRIELSANTOSDESOUZA_ExeVetor.java
+++ buscaBinaria/src/GABRIELSANTOSDESOUZA_ExeVetor.java
@@ -72,20 +72,57 @@ public class GABRIELSANTOSDESOUZA_ExeVetor {
     
     public static void randomNumbers(int arrayP[]){
         Random aleatorio = new Random();
-        int num = aleatorio.nextInt(10000);
+        int num = aleatorio.nextInt(49) +1;
         for(int i=0; i< arrayP.length; i++){
-            if(arrayP[i] == num){
-                break;
-            }
-            
+            arrayP[i] = num;
+            num = aleatorio.nextInt(49) + arrayP[i];
         }
     }
 
+    public static void binarySearch(int arrayP[], int value, boolean compare){
+        int start, end, middle, counting;
+        counting = 0;
+        start = 0;
+        end = arrayP.length-1;
+        middle = (start+end+1)/2;
+        while(start <= end) {
+            counting++;
+            middle = (start+end+1)/2;
+            if(value == arrayP[middle]) {
+                System.out.println("O valor " + value + " foi encontrado na posição: " + middle);
+                if(compare == true){
+                    System.out.println("Número de buscas binárias: " + counting);
+                }
+                return;
+            }
+
+            if (value < arrayP[middle]) {
+                end = middle - 1;
+            }
+            if (value > arrayP[middle]) {
+                start = middle +1;
+            }
+        }
+        System.out.println("O valor não existe no vetor");
+    }
+
+    public static void compareMethods(int[] arrayP, int value, boolean compare){
+        compare = true;
+        binarySearch(arrayP, value, compare);
+        int counting = 0;
+        for(int i =0; i < arrayP.length; i++){
+            counting++;
+            if(value == arrayP[i]){
+                System.out.println("Valor encontrado na posição: " + i);
+                System.out.println("Número de buscas: " + counting);
+            }
+        }
+    }
     public static void main(String[] args){
         Scanner tec = new Scanner(System.in);
         int array[] = new int[10000];
         String program = "rodando";
-        
+        boolean compare =false;
         while(program == "rodando"){
             System.out.println(
             "1) Zerar vetor\n" + 
@@ -98,6 +135,7 @@ public class GABRIELSANTOSDESOUZA_ExeVetor {
             "8) Encerrar programa\n" +
             "9) Gerar números aleatórios\n"+
             "10) Busca binária\n"
+            "11) Comparar buscas \n"
             );
             System.out.print("Digite o comando a ser realizado -> ");
             int task = tec.nextInt();
@@ -128,6 +166,16 @@ public class GABRIELSANTOSDESOUZA_ExeVetor {
                     break;
                 case 9:
                     randomNumbers(array);
+                    break;
+                case 10:
+                    System.out.print("Digite o número a ser procurado: ");
+                    int value = tec.nextInt();
+                    binarySearch(array, value, compare);
+                    break;
+                case 11:
+                    System.out.println("Digite o valor a ser procurado: ");
+                    value = tec.nextInt();
+                    compareMethods(array, value, compare);
                     break;
                 default: 
                     System.out.println("Comando invalido");

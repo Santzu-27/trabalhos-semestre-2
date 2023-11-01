4b36fb48f3fe838ea40080a024b78dbfb32f543b
Santzu-27 <gabi260402@gmail.com>
2023-11-01 07:31:47 -0300

    m

--- objetoVetor/src/App.java
+++ objetoVetor/src/App.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class App {
-    public static void main(String[] args) throws Exception {
+    public int scanInt(){
+        Scanner tec = new Scanner(System.in);
+        int n = tec.nextInt();
+        return n;
+    }
+
+    public static void main(String[] args){
         System.out.println("Hello, World!");
     }
 }
---
+++ objetoVetor/src/Vetor.java
@@ -0,0 +1,9 @@
+public class Vetor {
+    int n;
+    int[] array = new int[n];
+    public Vetor(int aLength){
+        this.n = aLength;
+    }
+        
+}    
+

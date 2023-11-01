7dda863189f9f38830e0470896f6fe6c1b854736
Santzu-27 <gabi260402@gmail.com>
2023-11-01 14:05:58 -0300

    Novo arquivo

--- objetoVetor/src/App.java
+++ objetoVetor/src/App.java
@@ -2,13 +2,46 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class App {
-    public int scanInt(){
+    static int scanInt(){
         Scanner tec = new Scanner(System.in);
         int n = tec.nextInt();
         return n;
     }
+    public void menu(){
+        System.out.println(
+                "01. Preencher o vetor do objeto com números pseudo-randômicos, em uma distribuição que minimize a possibilidade de valores duplicados.\n" +
+                "\n" +
+                "02. Preencher o vetor do objeto com números pseudo-randômicos em ordem crescente;\n" +
+                "\n" +
+                "03. Informar o total de elementos válidos no vetor do objeto;\n" +
+                "\n" +
+                "04. Informar o maior e o menor valores existentes no vetor do objeto;\n" +
+                "\n" +
+                "05. Listar todo o conteúdo do vetor do objeto;\n" +
+                "\n" +
+                "11. Inserir um valor no vetor do objeto em uma determinada posição.\n" +
+                "\n" +
+                "12. Apagar um valor do vetor do objeto, informando o valor a ser removido;\n" +
+                "\n" +
+                "13. Apagar um valor do vetor do objeto, informando a posição do valor a ser removido.\n" +
+                "\n" +
+                "21. Consultar a existência de um valor no vetor do objeto, utilizando busca sequencial;\n" +
+                "\n" +
+                "22. Consultar a existência de um valor no vetor do objeto, utilizando busca binária;\n" +
+                "\n" +
+                "31. Ordenar o vetor do objeto, utilizando o método Bubblesort;\n" +
+                "\n" +
+                "32. Ordenar o vetor do objeto, utilizando o método Insertionsort;\n" +
+                "\n" +
+                "33. Ordenar o vetor do objeto, utilizando o método Selectionsort.\n" +
+                "\n" +
+                "34. Ordenar o vetor do objeto, utilizando o método Quicksort.\n" +
+                "\n" +
+                "Classe que define o Objeto desta aplicação:");
+    }
 
     public static void main(String[] args){
-        System.out.println("Hello, World!");
+        System.out.print("Digite o tamanho do vetor: ");
+        Vetor array = new Vetor(scanInt());
     }
 }
--- objetoVetor/src/Vetor.java
+++ objetoVetor/src/Vetor.java
@@ -1,9 +1,9 @@
 public class Vetor {
     int n;
-    int[] array = new int[n];
+    int[]array;
     public Vetor(int aLength){
         this.n = aLength;
+        this.array = new int[n];
     }
-        
 }    
 

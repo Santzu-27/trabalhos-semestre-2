136253510a507bd74a7992f967d0360d76c05cb4
Santzu-27 <gabi260402@gmail.com>
2023-11-03 19:25:07 -0300

    commit

---
+++ testes/src/Main.java
@@ -0,0 +1,20 @@
+import java.io.FileInputStream;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        FileInputStream arquivo = new FileInputStream("src/arquivo.txt");
+        Scanner entradaTexto = new Scanner(arquivo);
+        String linha;
+        String [] SL;
+
+        while (entradaTexto.hasNextLine()){
+            linha = entradaTexto.nextLine();
+            System.out.println(linha);
+            SL = linha.split(",");
+            System.out.println(SL[0]);
+            System.out.println(SL[1]);
+            System.out.println(SL[2]);
+        }
+    }
+}

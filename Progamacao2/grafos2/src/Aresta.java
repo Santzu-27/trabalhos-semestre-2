1e51ae34c1076724bf9038c4685c9647a916324d
Gabriel <gabi260402@gmail.com>
2023-12-11 17:08:38 -0300

    main

---
+++ Progamacao2/grafos2/src/Aresta.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+public class Aresta {
+    Vertice cidade1, cidade2;
+    int distancia;
+
+    public void info_aresta(){
+        
+    }
+}
---
+++ Progamacao2/grafos2/src/Vertice.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Vertice{
+    String nomeCidade;
+    ArrayList<Vertice> vizinhança;
+    ArrayList<Aresta> conexoes;
+
+    public void info_vizinhos(){
+        
+    }
+
+    public void info_conexoes(){
+
+    }
+
+    public void info_vertice(){
+
+    }
+}

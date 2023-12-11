b7f40a40f8ac1bc4ad164d6aad4d48ef71b7a54a
Gabriel <gabi260402@gmail.com>
2023-12-11 16:33:33 -0300

    Commited

---
+++ Progamacao2/grafos/src/Grafo.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Grafo<TIPO> {
+    ArrayList<TIPO> cidades;
+    ArrayList<TIPO> conexoes;
+
+    public Grafo(){
+        this.cidades = new ArrayList<TIPO>();
+        this.conexoes = new ArrayList<TIPO>();
+    }
+
+    public void cadastra_cidade(){
+
+    }
+    public void cadastra_conexao(){
+
+    }
+
+    public void info_cidades(){
+        
+    }
+    
+    public void info_conexoes(){
+        
+    }
+}
